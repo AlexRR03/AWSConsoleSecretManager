83236e7454278763bcdd0ec28a01bf701fb93bec
AlexRR03 <alexciempo03@gmail.com>
2025-05-20 11:51:28 +0200

    Add project files.

---
+++ AWSConsoleSecretManager/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AWSConsoleSecretManager.Helpers;
+
+Console.WriteLine("Ejemplo Secret Manager");
+
+string miSecreto = await HelperSecretManager.GetSecretAsync();
+Console.WriteLine($"El secreto es: {miSecreto}");
+Console.WriteLine("Presione una tecla para continuar...");
+Console.ReadLine();
+
+
