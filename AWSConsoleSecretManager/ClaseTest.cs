3d1667f526877efc07d9875bdc1b74827c74580f
AlexRR03 <alexciempo03@gmail.com>
2025-05-20 13:41:21 +0200

    Obtener claves mediante inyeccion de dependencias

---
+++ AWSConsoleSecretManager/ClaseTest.cs
@@ -0,0 +1,22 @@
+﻿using AWSConsoleSecretManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSConsoleSecretManager
+{
+    public class ClaseTest
+    {
+        private KeysModel keysModel;
+        public ClaseTest(KeysModel keysModel)
+        {
+            this.keysModel = keysModel;
+        }
+        public string GetApiVulue()
+        {
+            return keysModel.Api;
+        }
+    }
+}
--- AWSConsoleSecretManager/Program.cs
+++ AWSConsoleSecretManager/Program.cs
@@ -1,10 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
 using AWSConsoleSecretManager.Helpers;
+using AWSConsoleSecretManager.Models;
+using Microsoft.Extensions.DependencyInjection;
+using AWSConsoleSecretManager;
 
 Console.WriteLine("Ejemplo Secret Manager");
 
 string miSecreto = await HelperSecretManager.GetSecretAsync();
-Console.WriteLine($"El secreto es: {miSecreto}");
+KeysModel model = JsonConvert.DeserializeObject<KeysModel>(miSecreto);
+Console.WriteLine($"MySql: {model.MySql}");
+Console.WriteLine($"Bucket: {model.Bucket}");
+Console.WriteLine($"Api: {model.Api}");
+
+//Almacenar el objeto keysmodel dentro de builder para recuperarlo donde sea
+var provider = new ServiceCollection()
+    .AddTransient<ClaseTest>()
+    .AddSingleton<KeysModel>(x=> model)
+    .BuildServiceProvider();
+var test = provider.GetService<ClaseTest>();
+Console.WriteLine($"Api desde el test: {test.GetApiVulue()}");
+
 Console.WriteLine("Presione una tecla para continuar...");
 Console.ReadLine();
 
